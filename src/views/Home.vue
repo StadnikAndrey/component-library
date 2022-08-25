8392021adc8727f63b76b824a639a1848251718b
MASTER <9346@ukr.net>
2022-08-25 22:03:31 +0300

    add link on repo on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,16 @@
-<template>
-  <!-- <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div> -->
+<template>   
    <h1 class="home__title">Component library for Vue.js version 3</h1>    
    <p class="home__subtitle">Custom components and components installed from npm. </p>
    <div class="home__txt">
-    <p>Component code can be found <a class="link" href="#">here.</a></p>
+    <p>Component code can be found <a class="link" href="https://github.com/StadnikAndrey/component-library">here.</a></p>
   </div>
     
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'   
 }
 </script>
 <style  lang="scss"> 
