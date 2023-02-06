b90c61d917dcd447aa8cccbf3b09b0dcc47977dd
MASTER <9346@ukr.net>
2023-02-06 20:43:20 +0200

    fix style Table1.vue

--- src/views/Table1.vue
+++ src/views/Table1.vue
@@ -4,6 +4,7 @@
     <ul class="table-txt__ul">
       <li>adaptive for different screen sizes: scrolling in two directions</li>
       <li>fixed table header and the first cell of the row</li>
+      <li>table width - by content</li>
     </ul>
   </div>
 
@@ -41,32 +42,30 @@
       </thead>
       <tbody>
         <tr v-for="(row, index) in table" :key="index">
-          <td scope="row">
-            {{ row.firstName }} {{row.lastName}}
-          </td>          
-          <td>{{ row.maidenName }}</td>   
-          <td>{{row.age}}</td>
-          <td>{{row.gender}}</td>
-          <td>{{row.email}}</td>
-          <td>{{row.phone}}</td>
-          <td>{{row.username}}</td>
-          <td>{{row.birthDate}}</td>
-          <td>{{row.image}}</td>
-          <td>{{row.bloodGroup}}</td>
-          <td>{{row.height}}</td>
-          <td>{{row.weight}}</td>
-          <td>{{row.eyeColor}}</td>
-          <td>{{row.hair.color}}</td>
-          <td>{{row.domain}}</td>
-          <td>{{row.ip}}</td>
-          <td>{{row.address.address}}</td>
-          <td>{{row.address.city}}</td>
-          <td>{{row.address.postalCode}}</td>
-          <td>{{row.address.state}}</td>
-          <td>{{row.macAddress}}</td>
-          <td>{{row.university}}</td>
-          <td>{{row.ein}}</td>
-          <td>{{row.ssn}}</td>           
+          <td scope="row">{{ row.firstName }} {{ row.lastName }}</td>
+          <td>{{ row.maidenName }}</td>
+          <td>{{ row.age }}</td>
+          <td>{{ row.gender }}</td>
+          <td>{{ row.email }}</td>
+          <td>{{ row.phone }}</td>
+          <td>{{ row.username }}</td>
+          <td>{{ row.birthDate }}</td>
+          <td>{{ row.image }}</td>
+          <td>{{ row.bloodGroup }}</td>
+          <td>{{ row.height }}</td>
+          <td>{{ row.weight }}</td>
+          <td>{{ row.eyeColor }}</td>
+          <td>{{ row.hair.color }}</td>
+          <td>{{ row.domain }}</td>
+          <td>{{ row.ip }}</td>
+          <td>{{ row.address.address }}</td>
+          <td>{{ row.address.city }}</td>
+          <td>{{ row.address.postalCode }}</td>
+          <td>{{ row.address.state }}</td>
+          <td>{{ row.macAddress }}</td>
+          <td>{{ row.university }}</td>
+          <td>{{ row.ein }}</td>
+          <td>{{ row.ssn }}</td>
         </tr>
       </tbody>
     </table>
@@ -132,16 +131,18 @@ li {
   padding: 2px 0;
 }
 ////////////////////////
-.wrap-table {
+.wrap-table {  
   overflow: auto;
-  max-height: 60vh;
+  max-width: fit-content;
+  max-height: 65vh;
   border: 1px solid #d6d6d6;
-  margin-bottom: 15px;
+  margin-bottom: 5px;
 
   scrollbar-width: thin;
   scrollbar-color: #ccc transparent;
   &::-webkit-scrollbar {
     width: 8px;
+    height: 8px;
   }
   &::-webkit-scrollbar-track {
     background: rgb(241, 239, 239);
@@ -154,9 +155,9 @@ li {
 }
 
 .table {
+  table-layout: fixed;   
   border-spacing: 0;
   margin: 0;
-  width: 100%;   
   color: #000;
 }
 
@@ -210,5 +211,4 @@ th[scope="row"] {
   z-index: map-get($zindex, scope_row);
   border-right: 2px solid rgb(175 205 167);
 }
- 
 </style>
