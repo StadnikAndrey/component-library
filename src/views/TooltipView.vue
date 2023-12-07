a1b8eafb701163fe11aed845ad8c280e2b06d2a3
Stadnik <stadnik.email@gmail.com>
2023-12-07 14:56:19 +0200

    fix tooltip

--- src/views/TooltipView.vue
+++ src/views/TooltipView.vue
@@ -23,7 +23,7 @@
     <div
       class="tooltip-view__item"
       @pointerenter="tooltip($event)"
-      data-tooltip="text tooltip"
+      data-toolti_p="text tooltip"
     >
       <span>tooltip</span>
     </div>
@@ -31,7 +31,7 @@
      <div
       class="tooltip-view__item"
       @pointerenter="tooltip($event)"
-      data-tooltip="2 text tooltip"
+      data-toolti_p="<p>2 text tooltip</p><p>txt txt</p>"
     >
       <span>2 tooltip</span>
     </div>
@@ -43,48 +43,75 @@ export default {
   name: "Tooltip",
   methods: {
     tooltip(e) {     
-      let touchDevice = "ontouchstart" in document.documentElement;
-      let tooltipElem;
-      let coords;
-      let top;
+       let touchDevice = "ontouchstart" in document.documentElement;
+			let tooltipElem;
+			let tooltipArrowElem;
+			let coords;
+			let top = 'auto';
+			let bottom;
+			let left = 'auto';
+			let right = 'auto';
 
-      let of = true;
-      let data = e.target.dataset.tooltip;
+			let of = true;
+			let data = e.target.dataset.toolti_p;
 
-      if (data != "" && of) {
-        of = false;
-        tooltipElem = document.createElement("div");
-        tooltipElem.className = "tooltip";
-        tooltipElem.insertAdjacentHTML("afterbegin", data);
-        e.target.append(tooltipElem);
-        coords = e.target.getBoundingClientRect();
+			if (data != "" && of) {
+				of = false;
+				tooltipElem = document.createElement("div");
+				tooltipElem.className = "tooltip_dt";
+				tooltipElem.insertAdjacentHTML("afterbegin", data);
+				e.target.append(tooltipElem);
+				// create arrow
+				tooltipArrowElem = document.createElement("div");
+				tooltipArrowElem.className = "tooltip_dt-arrow";
+				e.target.append(tooltipArrowElem);
+				tooltipArrowElem.style.top = "-1px";
+				tooltipArrowElem.style.left = "4px";
 
-        tooltipElem.style.maxWidth = e.target.offsetWidth + "px";
-        top = -tooltipElem.offsetHeight - 5;
-        // if the hint is placed at the top, it will be displayed at the bottom
-        if (coords.top < tooltipElem.offsetHeight) {
-          top = e.target.offsetHeight + 5;
-        }
-        tooltipElem.style.left = 5 + "px";
-        tooltipElem.style.top = top + "px";
-      }     
-      if (tooltipElem && !touchDevice) {
-        e.target.addEventListener("pointerleave", removeTooltip);
-        tooltipElem.addEventListener("pointerenter", removeTooltip);
-      }else if (tooltipElem && touchDevice) {
-        document.documentElement.addEventListener("pointerenter", removeTooltip);         
-      }
 
-      function removeTooltip() {            
-        if (tooltipElem) {
-          tooltipElem.remove();
-          tooltipElem = null;
-          of = true;
-        } else {
-          document.documentElement.removeEventListener("pointerenter", removeTooltip);
-          e.target.removeEventListener("pointerleave", removeTooltip);
-        }
-      }
+				coords = e.target.getBoundingClientRect();
+
+				// tooltipElem.style.maxWidth = e.target.offsetWidth + "px";
+				bottom = e.target.offsetHeight + "px";
+
+				// if the hint is placed at the top, it will be displayed at the bottom
+				if (coords.top < tooltipElem.offsetHeight) {
+					bottom = 'auto';
+					top = e.target.offsetHeight + 'px';
+					tooltipArrowElem.className = "tooltip_dt-arrow-top";
+					tooltipArrowElem.style.top = "auto";
+					tooltipArrowElem.style.bottom = "-1px";
+				}
+
+				tooltipElem.style.top = top;
+				tooltipElem.style.bottom = bottom;
+
+				if ((coords.left + tooltipElem.offsetWidth + 0) >= document.documentElement.clientWidth) {
+					left = (coords.left + tooltipElem.offsetWidth) - document.documentElement.clientWidth + 2;
+					left = -left + 'px';
+				}
+				tooltipElem.style.left = left;
+				tooltipElem.style.right = right;
+			}
+			if (tooltipElem && !touchDevice) {
+				e.target.addEventListener("pointerleave", removeTooltip);
+				// tooltipElem.addEventListener("pointerenter", removeTooltip);
+			} else if (tooltipElem && touchDevice) {
+				document.documentElement.addEventListener("pointerenter", removeTooltip);
+			}
+
+			function removeTooltip() {
+				if (tooltipElem) {
+					tooltipElem.remove();
+					tooltipElem = null;
+					tooltipArrowElem.remove();
+					tooltipArrowElem = null;
+					of = true;
+				} else {
+					document.documentElement.removeEventListener("pointerenter", removeTooltip);
+					e.target.removeEventListener("pointerleave", removeTooltip);
+				}
+			}
     },
   },
 };
@@ -99,18 +126,57 @@ export default {
   }
 }
 
-.tooltip {
+// .tooltip {
+//   position: absolute;
+//   z-index: 9999;
+//   padding: 6px 9px;
+//   border: 1px solid #b3c9ce;
+//   border-radius: 5px;
+//   font-size: 15px;
+//   color: #fefefe;
+//   background: #000;
+//   -webkit-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
+//   -moz-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
+//   box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
+//   white-space: initial;
+// }
+.tooltip_dt {
+   position: absolute;
+   z-index: 9999;
+   padding: 6px 9px;
+   /* border: 1px solid #b3c9ce; */
+   /* border-radius: 5px; */
+   /* border-bottom-left-radius: 8px; */
+   font-size: 15px;
+   color: #fefefe;
+   background-color: #333333;
+   /* -webkit-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2); */
+   /* -moz-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2); */
+   /* box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2); */
+   white-space: initial;
+   text-align: left;
+   min-width: 320px;
+   max-width: 320px;
+ }
+
+ .tooltip_dt:hover {
+   cursor: initial;
+ }
+
+ .tooltip_dt-arrow {
+   position: absolute;
+   z-index: 9998;
+   border-top: 5px solid #333333;
+   border-right: 5px solid transparent;
+   border-bottom: 5px solid transparent;
+   border-left: 5px solid transparent;
+ }
+ .tooltip_dt-arrow-top {
   position: absolute;
-  z-index: 9999;
-  padding: 6px 9px;
-  border: 1px solid #b3c9ce;
-  border-radius: 5px;
-  font-size: 15px;
-  color: #fefefe;
-  background: #000;
-  -webkit-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
-  -moz-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
-  box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
-  white-space: initial;
+  z-index: 9998;
+  border-top: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-bottom: 5px solid #333333;
+  border-left: 5px solid transparent;
 }
 </style>
