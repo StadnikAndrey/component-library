100b8d010ed3cf5b14b1481190c0b4c4850cc44e
Stadnik <stadnik.email@gmail.com>
2023-12-07 15:21:10 +0200

    up

--- src/views/TooltipView.vue
+++ src/views/TooltipView.vue
@@ -28,10 +28,10 @@
       <span>tooltip</span>
     </div>
 
-     <div
+    <div
       class="tooltip-view__item"
       @pointerenter="tooltip($event)"
-      data-toolti_p="<p>2 text tooltip</p><p>txt txt</p>"
+      data-toolti_p="<p>2 text tooltip</p><p>txt txt</p> <p>txt txt</p><p>txt txt</p><p>txt txt</p><p>txt txt</p><p>txt txt</p>"
     >
       <span>2 tooltip</span>
     </div>
@@ -42,76 +42,88 @@
 export default {
   name: "Tooltip",
   methods: {
-    tooltip(e) {     
-       let touchDevice = "ontouchstart" in document.documentElement;
-			let tooltipElem;
-			let tooltipArrowElem;
-			let coords;
-			let top = 'auto';
-			let bottom;
-			let left = 'auto';
-			let right = 'auto';
+    tooltip(e) {
+      let touchDevice = "ontouchstart" in document.documentElement;
+      let tooltipElem;
+      let tooltipArrowElem;
+      let coords;
+      let top = "auto";
+      let bottom;
+      let left = "auto";
+      let right = "auto";
 
-			let of = true;
-			let data = e.target.dataset.toolti_p;
+      let of = true;
+      let data = e.target.dataset.toolti_p;
 
-			if (data != "" && of) {
-				of = false;
-				tooltipElem = document.createElement("div");
-				tooltipElem.className = "tooltip_dt";
-				tooltipElem.insertAdjacentHTML("afterbegin", data);
-				e.target.append(tooltipElem);
-				// create arrow
-				tooltipArrowElem = document.createElement("div");
-				tooltipArrowElem.className = "tooltip_dt-arrow";
-				e.target.append(tooltipArrowElem);
-				tooltipArrowElem.style.top = "-1px";
-				tooltipArrowElem.style.left = "4px";
+      if (data != "" && of) {
+        of = false;
+        tooltipElem = document.createElement("div");
+        tooltipElem.className = "tooltip_dt";
+        tooltipElem.insertAdjacentHTML("afterbegin", data);
+        e.target.append(tooltipElem);
+        // create arrow
+        tooltipArrowElem = document.createElement("div");
+        tooltipArrowElem.className = "tooltip_dt-arrow";
+        e.target.append(tooltipArrowElem);
+        tooltipArrowElem.style.top = "-1px";
+        tooltipArrowElem.style.left = "4px";
 
+        coords = e.target.getBoundingClientRect();
 
-				coords = e.target.getBoundingClientRect();
+        // tooltipElem.style.maxWidth = e.target.offsetWidth + "px";
+        bottom = e.target.offsetHeight + "px";
 
-				// tooltipElem.style.maxWidth = e.target.offsetWidth + "px";
-				bottom = e.target.offsetHeight + "px";
+        // if the hint is placed at the top, it will be displayed at the bottom
+        if (coords.top < tooltipElem.offsetHeight) {
+          bottom = "auto";
+          top = e.target.offsetHeight + "px";
+          tooltipArrowElem.className = "tooltip_dt-arrow-top";
+          tooltipArrowElem.style.top = "auto";
+          tooltipArrowElem.style.bottom = "-1px";
+        }
 
-				// if the hint is placed at the top, it will be displayed at the bottom
-				if (coords.top < tooltipElem.offsetHeight) {
-					bottom = 'auto';
-					top = e.target.offsetHeight + 'px';
-					tooltipArrowElem.className = "tooltip_dt-arrow-top";
-					tooltipArrowElem.style.top = "auto";
-					tooltipArrowElem.style.bottom = "-1px";
-				}
+        tooltipElem.style.top = top;
+        tooltipElem.style.bottom = bottom;
 
-				tooltipElem.style.top = top;
-				tooltipElem.style.bottom = bottom;
+        if (
+          coords.left + tooltipElem.offsetWidth + 0 >=
+          document.documentElement.clientWidth
+        ) {
+          left =
+            coords.left +
+            tooltipElem.offsetWidth -
+            document.documentElement.clientWidth +
+            2;
+          left = -left + "px";
+        }
+        tooltipElem.style.left = left;
+        tooltipElem.style.right = right;
+      }
+      if (tooltipElem && !touchDevice) {
+        e.target.addEventListener("pointerleave", removeTooltip);
+        // tooltipElem.addEventListener("pointerenter", removeTooltip);
+      } else if (tooltipElem && touchDevice) {
+        document.documentElement.addEventListener(
+          "pointerenter",
+          removeTooltip
+        );
+      }
 
-				if ((coords.left + tooltipElem.offsetWidth + 0) >= document.documentElement.clientWidth) {
-					left = (coords.left + tooltipElem.offsetWidth) - document.documentElement.clientWidth + 2;
-					left = -left + 'px';
-				}
-				tooltipElem.style.left = left;
-				tooltipElem.style.right = right;
-			}
-			if (tooltipElem && !touchDevice) {
-				e.target.addEventListener("pointerleave", removeTooltip);
-				// tooltipElem.addEventListener("pointerenter", removeTooltip);
-			} else if (tooltipElem && touchDevice) {
-				document.documentElement.addEventListener("pointerenter", removeTooltip);
-			}
-
-			function removeTooltip() {
-				if (tooltipElem) {
-					tooltipElem.remove();
-					tooltipElem = null;
-					tooltipArrowElem.remove();
-					tooltipArrowElem = null;
-					of = true;
-				} else {
-					document.documentElement.removeEventListener("pointerenter", removeTooltip);
-					e.target.removeEventListener("pointerleave", removeTooltip);
-				}
-			}
+      function removeTooltip() {
+        if (tooltipElem) {
+          tooltipElem.remove();
+          tooltipElem = null;
+          tooltipArrowElem.remove();
+          tooltipArrowElem = null;
+          of = true;
+        } else {
+          document.documentElement.removeEventListener(
+            "pointerenter",
+            removeTooltip
+          );
+          e.target.removeEventListener("pointerleave", removeTooltip);
+        }
+      }
     },
   },
 };
@@ -120,58 +132,38 @@ export default {
 .tooltip-view {
   height: 150vh;
   &__item {
-    position: relative;     
+    position: relative;
     max-width: 200px;
     margin-bottom: 50px;
   }
 }
 
-// .tooltip {
-//   position: absolute;
-//   z-index: 9999;
-//   padding: 6px 9px;
-//   border: 1px solid #b3c9ce;
-//   border-radius: 5px;
-//   font-size: 15px;
-//   color: #fefefe;
-//   background: #000;
-//   -webkit-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
-//   -moz-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
-//   box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2);
-//   white-space: initial;
-// }
 .tooltip_dt {
-   position: absolute;
-   z-index: 9999;
-   padding: 6px 9px;
-   /* border: 1px solid #b3c9ce; */
-   /* border-radius: 5px; */
-   /* border-bottom-left-radius: 8px; */
-   font-size: 15px;
-   color: #fefefe;
-   background-color: #333333;
-   /* -webkit-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2); */
-   /* -moz-box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2); */
-   /* box-shadow: 7px 8px 9px -4px rgba(34, 60, 80, 0.2); */
-   white-space: initial;
-   text-align: left;
-   min-width: 320px;
-   max-width: 320px;
- }
+  position: absolute;
+  z-index: 9999;
+  padding: 6px 9px;
+  font-size: 15px;
+  color: #fefefe;
+  background-color: #333333;
+  white-space: initial;
+  text-align: left;
+  min-width: 320px;
+  max-width: 320px;
+}
 
- .tooltip_dt:hover {
-   cursor: initial;
- }
+.tooltip_dt:hover {
+  cursor: initial;
+}
 
- .tooltip_dt-arrow {
-   position: absolute;
-   z-index: 9998;
-   border-top: 5px solid #333333;
-   border-right: 5px solid transparent;
-   border-bottom: 5px solid transparent;
-   border-left: 5px solid transparent;
- }
- .tooltip_dt-arrow-top {
+.tooltip_dt-arrow {
+  position: absolute;
+  z-index: 9998;
+  border-top: 5px solid #333333;
+  border-right: 5px solid transparent;
+  border-bottom: 5px solid transparent;
+  border-left: 5px solid transparent;
+}
+.tooltip_dt-arrow-top {
   position: absolute;
   z-index: 9998;
   border-top: 5px solid transparent;
